24c3182edcf94c2c9ef6c7aedc88adb5763621a3
mmyy2513 <mmyy2513@hanyang.ac.kr>
2021-05-25 16:47:10 +0900

    1

--- alarm.py
+++ alarm.py
@@ -9,7 +9,7 @@ import socket
 
 # socket init
 host = '192.168.1.3'
-port = 9999
+port = 9800
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
 s.bind((host,port))
@@ -26,23 +26,59 @@ firebase_admin.initialize_app(cred,{
 data = db.reference().get()
 name = next(iter(data.keys()))
 
-# get time to alarm
-alarm = int(db.reference(str(name) + '/medicine').get())
-print(alarm)
-
+meal_cnt = 0
+medicine_cnt = 0
 
 while True:
+    # get time to alarm
+    medicine_alarm = int(db.reference(str(name) + '/medicine').get())
+    medicine_alarm = str(medicine_alarm)
+    meal_alarm = int(db.reference(str(name) + '/meal').get())
+    meal_alarm = str(meal_alarm)
+
+    
+    mdc_hour = medicine_alarm[:-2]
+    mdc_min = medicine_alarm[-2:]
+    meal_hour = meal_alarm[:-2]
+    meal_min = meal_alarm[-2:]
+
+
     now = time.localtime()
     hour = now.tm_hour
     minute = now.tm_min
 
-    print(hour, type(hour))
-    print(minute, type(minute))
-
-    if hour == alarm and minute == 0:
-        print("its time")
-        # socket 1 send
+    
+    if hour == int(mdc_hour) and minute == int(mdc_min):
+        if medicine_cnt == 0:
+            conn.send("1".encode())
+            medicine_cnt = 1
+            while True:
+                if conn.recv(1024): break
+        else :
+            conn.send("0".encode())
+            while True:
+                if conn.recv(1024): break
     else:
-        print("not yet")
-        # socket 0 send
+        
+        medicine_cnt = 0
+        conn.send("0".encode())
+        while True:
+            if conn.recv(1024): break
+
+    if hour == int(meal_hour) and minute == int(meal_min):
+        if meal_cnt == 0:
+            conn.send("2".encode())
+            meal_cnt = 1
+            while True:
+                if conn.recv(1024): break
+        else :
+            conn.send("0".encode())
+            while True:
+                if conn.recv(1024): break
+    else:
+        
+        meal_cnt = 0
+        conn.send("0".encode())
+        while True:
+            if conn.recv(1024): break
 
